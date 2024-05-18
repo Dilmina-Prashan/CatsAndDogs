2a0c6d8d779ff7e4a3884a9b67aa4fe96a815cc2
Biman Gunarathne <bimangunarathne@outlook.com>
2024-05-18 16:45:51 +0530

    delete navbar style

--- components/navbar.php
+++ components/navbar.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <link rel="stylesheet" type="text/css" href="../styles/navbar.css">
-</head>
-
 <body>
     <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
@@ -16,7 +11,7 @@
             </div>
 
             <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2">Home</a></li>
+                <li><a href="../index.php" class="nav-link px-2">Home</a></li>
                 <li><a href="#" class="nav-link px-2">Products</a></li>
                 <li><a href="#" class="nav-link px-2">FAQs</a></li>
                 <li><a href="#" class="nav-link px-2">About</a></li>
