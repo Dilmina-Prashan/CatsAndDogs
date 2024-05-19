10dce8619464f10fb8f64b69226e02e54e450d32
Biman Gunarathne <bimangunarathne@outlook.com>
2024-05-19 08:38:52 +0530

    updated

--- components/footer.php
+++ components/footer.php
@@ -8,7 +8,6 @@
 
             <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                 <img src="../assets/cat-halloween-kitty-svgrepo-com.svg" width="40" alt="cat-logo">
-                <!-- <span class="fs-4">Cats & Dogs</span> -->
             </a>
 
             <ul class="nav col-md-4 justify-content-end">
