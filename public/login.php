1ddee6672d44b9c3467567ff24d8bc048d93bf8c
Biman Gunarathne <bimangunarathne@outlook.com>
2024-05-25 08:20:42 +0530

    update login page

--- public/login.php
+++ public/login.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/login.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Login</title>
 </head>
 
 <body>
     <section class="loginMain">
+        <a href="../public/product.php" class="bacbutton"><i class="bi bi-arrow-left-circle"></i></a>
         <div class="container">
             <div class="login">
                 <div class="row innerRow">
@@ -31,9 +33,12 @@
                                 </div>
                                 <div class="mb-3 form-check">
                                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                    <label class="form-check-label" for="exampleCheck1">Remember Me</label>
                                 </div>
-                                <a href="" class="btn btn-primary">Login</a>
+                                <div class="d-grid gap-2">
+                                    <a href="" class="btn btn-dark btn-sm">Login</a>
+                                    <a href="../public/signup.php" class="btn btn-outline-dark btn-sm">Signup</a>
+                                </div>
                             </form>
                         </div>
                     </div>
