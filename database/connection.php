b28ed31b5f422973446b102e6213a80f05cb1e32
bimangunarathne <bimangunarathne@outlook.com>
2024-06-22 08:51:35 +0530

    update connection

--- database/connection.php
+++ database/connection.php
@@ -5,8 +5,9 @@ $password = "";
 $db_name = "catsanddogs_db";
 $port = 3306;
 
-$conn = new mysqli($servername,$username,$password,$db_name, $port);
-if($conn->connect_error){
-    die("Connation Faild ".$conn->connect_error);
+$conn = new mysqli($servername, $username, $password, $db_name, $port);
+if ($conn->connect_error) {
+    die("Connation Faild " . $conn->connect_error);
+} else {
+    return $conn;
 }
-echo "";
